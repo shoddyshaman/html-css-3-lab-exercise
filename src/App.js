7fd13a1d79a5df9021a78aac08fbf90b37507149
shoddyshaman <djtiwari90@gmail.com>
2021-01-07 22:36:15 -0700

    uploading completed lab-basic requirements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React,{Component} from 'react'
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dropdownMenu:false
+    }
+  }
+  
+  toggleDropdown = () => {
+    this.setState({dropdownMenu: !this.state.dropdownMenu})
+  }
+
+  render(){
+    return (
+      <section className="App-body">
+        <div className="navbar">
+          <div className="container">
+              <img className="logo" src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg" alt="Start Bootstrap"/>
+              <nav className="desktop-menu">
+                <span>SERVICES</span>
+                <span>PORTFOLIO</span>
+                <span>ABOUT</span>
+                <span>TEAM</span>
+                <span>CONTACT</span>
+              </nav>
+              <div className="dropdown" onClick={this.toggleDropdown}><button className="btn-menu">MENU</button></div>
+              {this.state.dropdownMenu
+              ?(
+                <nav className="mobile-menu">
+                  <span>SERVICES</span>
+                  <span>PORTFOLIO</span>
+                  <span>ABOUT</span>
+                  <span>TEAM</span>
+                  <span>CONTACT</span>
+                </nav>
+              )
+            : null}
+          </div>
+        </div>
+        <header className="App-header">
+          <div className="container-2">
+            <h3>Welcome To Our Studio!</h3>
+            <h1>IT'S NICE TO MEET YOU</h1>
+            <button className="btn-tell-me-more">TELL ME MORE</button>
+          </div>
+        </header>
+      </section>
+    )
+  }
 }
 
 export default App;
